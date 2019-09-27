ca79b57becb5d71ddaf95b5e1f7ac60a8878f975
Italo Nunes Leite <italonleite@gmail.com>
2019-09-26 22:10:00 -0300

    - exibindo titulo das imagens encontradas e duplicadas

--- js/principal.js
+++ js/principal.js
@@ -1,94 +1,110 @@
 function alterarNivel() {
     var nivel = document.querySelector("#nivel").value;
     var resultado = BuscarImagens(nivel);
-     CriarPares(resultado)
-  
+    CriarPares(resultado);
+
 }
 
-function CriarPares(resultado){ 
-    const cloneResultado = [...resultado];
-    console.log("resultado", resultado);
-    console.log("clone", cloneResultado);
-}
+function CriarPares(resultado) {
+    var cloneResultado = [...resultado];
+    var ul = document.querySelector("#imagem");
+    for (var i = 0; i < resultado.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = resultado[i].titulo;
+        ul.appendChild(li);
 
+    }
+
+    for (var i = 0; i < cloneResultado.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = resultado[i].titulo;
+        ul.appendChild(li);
+    }
+}
 
 function BuscarImagens(nivel) {
     var resultado = [];
     var tamanho = 0;
-switch (nivel)
-{
-case "facil":
-tamanho = 3;
-break;
+    switch (nivel) {
+        case "facil":
+            tamanho = 3;
+            break;
 
-case "medio":
-tamanho = 6;
-break;
+        case "medio":
+            tamanho = 6;
+            break;
 
-case "dificil":
-tamanho = 9;
-break;
-}
+        case "dificil":
+            tamanho = 9;
+            break;
+    }
 
-for (var i = 0; i < tamanho; i++) {
-  resultado.push(imagensDiretorio[i]);
-}
+    for (var i = 0; i < tamanho; i++) {
+        resultado.push(imagensDiretorio[i]);
+    }
 
-return resultado;
+    return resultado;
 }
 
 
 var imagensDiretorio = [
     {
         id: 1,
-valor: 'basebal',
-        titulo: 'basebal',    
+        valor: 'basebal',
+        titulo: 'basebal',
     },
     {
         id: 2,
-valor: 'surfista',
-        titulo: 'surfista',    
+        valor: 'surfista',
+        titulo: 'surfista',
     },
     {
         id: 3,
-valor: 'ciclista',
-        titulo: 'ciclista',        
+        valor: 'ciclista',
+        titulo: 'ciclista',
     },
     {
         id: 4,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'bola',
+        titulo: 'bola',
     },
     {
         id: 5,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'papel',
+        titulo: 'papel',
     },
     {
         id: 6,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'rede',
+        titulo: 'rede',
     },
     {
         id: 7,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'tesoura',
+        titulo: 'tesoura',
     },
     {
         id: 8,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'computador',
+        titulo: 'computador',
     },
     {
-        id:9,
-valor: 'bola',
-        titulo: 'bola',      
+        id: 9,
+        valor: 'mouse',
+        titulo: 'mouse',
     },
     {
         id: 10,
-valor: 'bola',
-        titulo: 'bola',      
+        valor: 'teclado',
+        titulo: 'teclado',
     },
-   
+
 ]
+
+
+/*var card = document.querySelector('.card');
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+});*/
+
 
